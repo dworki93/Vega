2d8cf409da35697c8c50e189d2bd4a361e582829
dworki93 <bartosz.dworkiewicz@gmail.com>
2017-08-24 22:26:51 +0200

    Add vehicle controller with endpoints for makes and features

---
+++ Controllers/VehiclesController.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Vega.Data;
+using Vega.Resources;
+
+namespace Vega.Controllers
+{
+    public class VehiclesController : Controller
+    {
+        private readonly VegaDbContext _context;
+        private readonly IMapper _mapper;
+
+        public VehiclesController(VegaDbContext context, IMapper mapper)
+        {
+            _mapper = mapper;
+            _context = context;
+        }
+
+        [Route("api/makes")]
+        public async Task<IActionResult> GetMakes()
+        {
+            var makes = await _context.Makes
+                .Include(m => m.Models).ToListAsync();
+
+            return Ok(_mapper.Map<List<MakeResource>>(makes));
+        }
+
+        [Route("api/features")]
+        public async Task<IActionResult> GetFeatures()
+        {
+            var features = await _context.Features
+                .ToListAsync();
+
+            return Ok(_mapper.Map<List<FeatureResource>>(features));
+        }
+    }
+}
