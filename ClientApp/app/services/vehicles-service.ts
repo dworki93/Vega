66042c97feed1df55bb83537ee953218d55d371d
dworki93 <bartosz.dworkiewicz@gmail.com>
2017-08-25 13:25:03 +0200

    Create vehicle service with method for getting makes and features

---
+++ ClientApp/app/services/vehicles-service.ts
@@ -0,0 +1,21 @@
+import { Feature } from './../../models/feature';
+import { Injectable } from "@angular/core";
+import { Http } from "@angular/http";
+import { Observable } from "rxjs/Observable";
+import 'rxjs/add/operator/map';
+import { Make } from "../../models/make";
+
+@Injectable()
+export class VehiclesService {
+    constructor(private _http: Http) {}
+
+    getMakes() : Observable<Array<Make>> {
+        return this._http.get('api/makes')
+            .map(res => res.json());
+    }
+
+    getFeatures() : Observable<Array<Feature>> {
+        return this._http.get('api/features')
+            .map(res => res.json());
+    }
+}
