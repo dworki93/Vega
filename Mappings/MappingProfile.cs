24550c7e52d9189f3bda6ba3bb5b9c6d5334e156
dworki93 <bartosz.dworkiewicz@gmail.com>
2017-08-25 17:01:02 +0200

    Create resource for vehicle

--- Mappings/MappingProfile.cs
+++ Mappings/MappingProfile.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using AutoMapper;
 using Vega.Domain;
 using Vega.Resources;
@@ -11,6 +13,31 @@ namespace Vega.Mappings
             CreateMap<Feature, FeatureResource>();
             CreateMap<Model, ModelResource>();
             CreateMap<Make, MakeResource>();
+
+            CreateMap<VehicleResource, Vehicle>()
+                .ForMember(v => v.Id, opt => opt.Ignore())
+                .ForMember(v => v.Features, opt => opt.Ignore())
+                .ForMember(v => v.ContactEmail, opt => opt.MapFrom(vr => vr.Contact.Email))
+                .ForMember(v => v.ContactName, opt => opt.MapFrom(vr => vr.Contact.Name))
+                .ForMember(v => v.ContactPhone, opt => opt.MapFrom(vr => vr.Contact.Phone))
+                .AfterMap((vr, v) => {
+                    var removedFeatures = v.Features
+                        .Where(f => !vr.Features.Contains(f.FeatureId));
+                    
+                    foreach (var f in removedFeatures)
+                        v.Features.Remove(f);
+
+                    var addedFeatures = vr.Features
+                        .Where(id => !v.Features.Any(f => f.FeatureId == id))
+                        .Select(id => new VehicleFeature { FeatureId = id });  
+
+                    foreach (var f in addedFeatures)                       
+                        v.Features.Add(f);                
+                });
+
+            CreateMap<Vehicle, VehicleResource>()
+                .ForMember(vr => vr.Features, opt => opt.MapFrom(v => v.Features.Select(f => f.FeatureId)))
+                .ForMember(vr => vr.Contact, opt => opt.MapFrom(v => new ContactResource { Phone = v.ContactPhone, Name = v.ContactName, Email = v.ContactEmail }));
         }
     }
 }
---
+++ Resources/VehicleResource.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vega.Resources
+{
+    public class VehicleResource
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int ModelId { get; set; }
+
+        public bool IsRegistered { get; set; }
+
+        public ICollection<int> Features { get; set; }
+
+        [Required]
+        public ContactResource Contact { get; set; }
+
+        public VehicleResource()
+        {
+            Features = new Collection<int>();
+        }
+    }
+}
